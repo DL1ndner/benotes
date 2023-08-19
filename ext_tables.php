31f68a9d5cbfd6b4b8547a67a6ad45d9014423b7
DL1ndner <35764776+DL1ndner@users.noreply.github.com>
2023-08-19 21:36:50 +0200

    Update ext_tables.php

--- ext_tables.php
+++ ext_tables.php
@@ -5,8 +5,4 @@ $extensionKey = 'benotes';
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($extensionKey, 'Configuration/TypoScript', 'Backend Notes');
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_benotes_domain_model_note');
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_benotes_domain_model_category');
-
 ?>
