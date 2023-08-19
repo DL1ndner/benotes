ab5984aac3e16d0b34086a4ec6f2bace2e2424dc
DL1ndner <35764776+DL1ndner@users.noreply.github.com>
2023-08-19 22:48:37 +0200

    Update CategoryController.php

--- Classes/Controller/CategoryController.php
+++ Classes/Controller/CategoryController.php
@@ -32,6 +32,8 @@ use TYPO3\CMS\Backend\View\BackendTemplateView;
 use TYPO3\CMS\Extbase\Mvc\View\ViewInterface;
 use TYPO3\CMS\Core\Imaging\Icon;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Backend\Attribute\Controller;
+use TYPO3\CMS\Backend\Template\ModuleTemplateFactory;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
 use TYPO3\CMS\Extbase\Mvc\Web\Routing\UriBuilder;
 use TYPO3\CMS\Backend\Template\ModuleTemplate;
