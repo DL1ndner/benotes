af52d977f0b6b3e894e88bd4bea5b623e785b005
DL1ndner <35764776+DL1ndner@users.noreply.github.com>
2023-08-19 21:31:02 +0200

    remove deprecated calls #97312

--- ext_tables.php
+++ ext_tables.php
@@ -5,10 +5,8 @@ $extensionKey = 'benotes';
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($extensionKey, 'Configuration/TypoScript', 'Backend Notes');
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_benotes_domain_model_note', 'EXT:benotes/Resources/Private/Language/locallang_csh_tx_benotes_domain_model_note.xlf');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_benotes_domain_model_note');
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_benotes_domain_model_category', 'EXT:benotes/Resources/Private/Language/locallang_csh_tx_benotes_domain_model_category.xlf');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_benotes_domain_model_category');
 
 ?>
