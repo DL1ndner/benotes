f5f23a97f707f1fd670454798405418a0e5f5365
DL1ndner <35764776+DL1ndner@users.noreply.github.com>
2023-08-19 21:59:46 +0200

    Delete ext_tables.php

--- ext_tables.php
+++
@@ -1,8 +0,0 @@
-<?php
-defined('TYPO3') or die();
-
-$extensionKey = 'benotes';
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($extensionKey, 'Configuration/TypoScript', 'Backend Notes');
-
-?>
