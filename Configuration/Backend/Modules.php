797007bdc571ec6014e1ccb368eabc0259907329
DL1ndner <35764776+DL1ndner@users.noreply.github.com>
2023-08-19 21:27:11 +0200

    Create Modules.php

---
+++ Configuration/Backend/Modules.php
@@ -0,0 +1,37 @@
+<?php
+return [
+    'user_benotes' => [
+        'parent' => 'user',
+        'position' => ['before' => '*'],
+        'access' => 'user',
+        'workspaces' => 'live',
+        'iconIdentifier' => 'notes',
+        'path' => '/module/user/benotes',
+        'labels' => 'LLL:EXT:benotes/Resources/Private/Language/locallang.xlf',
+        'extensionName' => 'benotes',
+        'controllerActions' => [
+            NoteController::class => [
+                'list',
+                'listPrivate', 
+                'show', 
+                'new',
+                'create',
+                'edit', 
+                'update',
+                'delete'
+            ],
+            CategoryController::class => [
+                'list',
+                'listPrivate', 
+                'show', 
+                'new',
+                'create',
+                'edit', 
+                'update',
+                'delete'
+            ],
+        ],
+    ],
+];
+
+?>
