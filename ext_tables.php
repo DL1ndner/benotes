e49e877461ce922eaecc40389d63b0d0479e5e57
DL1ndner <35764776+DL1ndner@users.noreply.github.com>
2023-08-19 21:28:19 +0200

    move module registration to Modules.php

--- ext_tables.php
+++ ext_tables.php
@@ -2,25 +2,6 @@
 defined('TYPO3') or die();
 
 $extensionKey = 'benotes';
-/**
- * Registers a Backend Module
- */
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
-	'Benotes',
-	'user',	 // Make module a submodule of 'user'
-	'Notes',	// Submodule key
-	'top',						// Position
-	[
-	Dl\Benotes\Controller\NoteController::class => 'list, listPrivate, show, new, create, edit, update, delete',
-	Dl\Benotes\Controller\CategoryController::class => 'list, listPrivate, show, new, create, edit, update, delete'
-	],
-	
-	[
-		'access' => 'user,group',
-		'icon'   => 'EXT:benotes/Resources/Public/Icons/tx_benotes_domain_model_notes.png',
-		'labels' => 'LLL:EXT:benotes/Resources/Private/Language/locallang_notes.xlf',
-	]
-);
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($extensionKey, 'Configuration/TypoScript', 'Backend Notes');
 
