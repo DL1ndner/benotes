93da18c90c318f8d460cf0d9a6962c3b19874642
DL1ndner <35764776+DL1ndner@users.noreply.github.com>
2023-08-19 20:51:53 +0200

    Remove TBE styles

--- ext_tables.php
+++ ext_tables.php
@@ -22,19 +22,12 @@ $extensionKey = 'benotes';
 	]
 );
 
-
-
-$GLOBALS['TBE_STYLES']['skins'][$_EXTKEY]['stylesheetDirectories'][] = 'EXT:benotes/Resources/Public/css/tx_benotes.css';
-
-
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($extensionKey, 'Configuration/TypoScript', 'Backend Notes');
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_benotes_domain_model_note', 'EXT:benotes/Resources/Private/Language/locallang_csh_tx_benotes_domain_model_note.xlf');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_benotes_domain_model_note');
 
-
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_benotes_domain_model_category', 'EXT:benotes/Resources/Private/Language/locallang_csh_tx_benotes_domain_model_category.xlf');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_benotes_domain_model_category');
-
 
 ?>
