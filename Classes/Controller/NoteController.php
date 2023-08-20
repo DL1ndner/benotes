1659890e32ae578f0ebc20f24829b29fd3e4b10e
DL1ndner <35764776+DL1ndner@users.noreply.github.com>
2023-08-20 20:21:16 +0200

    Update NoteController.php

--- Classes/Controller/NoteController.php
+++ Classes/Controller/NoteController.php
@@ -56,7 +56,7 @@ use Dl\Benotes\Domain\Repository\CategoryRepository;
 final class NoteController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController {
 
 	public function __construct(
-         protected readonly ModuleTemplateFactory $moduleTemplateFactory,
+         protected readonly ModuleTemplateFactory $moduleTemplateFactory
     	 ) {
 	}
 
