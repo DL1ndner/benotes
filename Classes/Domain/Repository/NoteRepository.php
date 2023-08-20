30f9bc7311cd94bc670fd2eb45e3eb04ad9f700f
DL1ndner <35764776+DL1ndner@users.noreply.github.com>
2023-08-20 22:46:09 +0200

    Update NoteRepository.php

--- Classes/Domain/Repository/NoteRepository.php
+++ Classes/Domain/Repository/NoteRepository.php
@@ -31,6 +31,17 @@ namespace Dl\Benotes\Domain\Repository;
  */
 class NoteRepository extends \TYPO3\CMS\Extbase\Persistence\Repository  {
 
+	/**
+	 * Initialize the repository
+	 *
+	 * @return void
+	 */
+	public function initializeObject() {
+		$querySettings = $this->objectManager->get('TYPO3\\CMS\\Extbase\\Persistence\\Generic\\Typo3QuerySettings');
+		$querySettings->setRespectStoragePage(TRUE);
+		$defaultQuerySettings = $this->createQuery()->getQuerySettings();
+	}
+
 	
 	/**
 	 * Find notes by given pids and author
