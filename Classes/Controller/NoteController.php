f41bb059f29d534528ff4b2cd02c0ff2f0b7db8c
DL1ndner <35764776+DL1ndner@users.noreply.github.com>
2023-08-23 16:34:31 +0200

    fix typo

--- Classes/Controller/NoteController.php
+++ Classes/Controller/NoteController.php
@@ -138,7 +138,7 @@ class NoteController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController 
 		   	]
 		);
 		$this->view->assign('notes', $notes);	
-		/$moduleTemplate = $this->moduleTemplateFactory->create($this->request);
+		$moduleTemplate = $this->moduleTemplateFactory->create($this->request);
         $moduleTemplate->setContent($this->view->render());
 	    return $this->htmlResponse($moduleTemplate->renderContent());
 	}
