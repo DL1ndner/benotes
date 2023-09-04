9b1440ded5d7a3794210aca522f25c767a0e09df
DL1ndner <35764776+DL1ndner@users.noreply.github.com>
2023-09-04 22:39:28 +0200

    Update NoteController.php

--- Classes/Controller/NoteController.php
+++ Classes/Controller/NoteController.php
@@ -169,7 +169,7 @@ class NoteController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController 
 		
 		$this->view->assign('notes', $notes);	
 		$moduleTemplate = $this->moduleTemplateFactory->create($this->request);
-		$moduleTemplate->setDocHeader();
+		$view->setDocHeader();
       		$moduleTemplate->setContent($this->view->render());
 		return $this->htmlResponse($moduleTemplate->renderContent());
 
