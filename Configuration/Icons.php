076b7a0ff25d612ad133fe0a94afe03b3d6bd282
DL1ndner <35764776+DL1ndner@users.noreply.github.com>
2023-09-04 21:45:36 +0200

    Create Icons.php

---
+++ Configuration/Icons.php
@@ -0,0 +1,7 @@
+<?php
+return [
+  'moduleicon' => [
+    'provider' => \TYPO§\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
+    'source' => 'EXT:benotes/Resources/Public/Icons/tx_benotes_domain_model_notes.png',
+  ],
+];
