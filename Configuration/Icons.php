9889f53caff0a898ef6c32f589b06038b71e2a5d
DL1ndner <35764776+DL1ndner@users.noreply.github.com>
2023-09-04 21:49:53 +0200

    Update Icons.php

--- Configuration/Icons.php
+++ Configuration/Icons.php
@@ -1,6 +1,6 @@
 <?php
 return [
-  'moduleicon' => [
+  'notes' => [
     'provider' => \TYPO§\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
     'source' => 'EXT:benotes/Resources/Public/Icons/tx_benotes_domain_model_notes.png',
   ],
