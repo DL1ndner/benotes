9d29876dede977c5220a9cc22d8e7061e22df34f
DL1ndner <35764776+DL1ndner@users.noreply.github.com>
2023-09-05 13:41:35 +0200

    Update NoteController.php

--- Classes/Controller/NoteController.php
+++ Classes/Controller/NoteController.php
@@ -82,8 +82,9 @@ class NoteController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController 
 	)  
 	{
 	 	$this->moduleName = 'benotes_note';
-        $this->modulePrefix = 'tx_benotes_user_benotesnotes';
+        	$this->modulePrefix = 'tx_benotes_user_benotesnotes';
 		$this->moduleTemplateFactory = $moduleTemplateFactory;
+		$this->iconFactory = GeneralUtility::makeInstance(IconFactory::class);
    	}
 
 	public function injectNoteRepository(NoteRepository $noteRepository)
@@ -108,10 +109,231 @@ class NoteController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController 
 	protected function initializeAction()
 	{
 		$pageRenderer = GeneralUtility::makeInstance(PageRenderer::class);
-
+        	$pageRenderer->addCssFile('EXT:benotes/Resources/Public/css/tx_benotes.css');
 		$this->moduleTemplate = $this->moduleTemplateFactory->create($this->request);
 		$this->moduleTemplate->setTitle('EXT:benotes');
 	}
+
+	/**
+     * @param ButtonBar $buttonBar
+     * @param string $type
+     * @param array $params
+     * @param bool $disabled
+     * @return ButtonBar
+     * @throws Exception
+     */
+    protected function getButton(ButtonBar $buttonBar, string $type, array $params = [], bool $disabled=false)
+    {
+        $translationKey = $params['translationKey'] ?? '';
+        $table = $params['table'] ?? '';
+        $uid = $params['uid'] ?? '';
+        $iconIdentifier = $params['iconIdentifier'] ?? '';
+        $action = $params['action'] ?? '';
+        $controller = $params['controller'] ?? 'Backend';
+        //
+        // Extension key (normalized)
+        $extensionName = $this->extensionName;
+        if (substr($extensionName, -3, 3) === 'Pro') {
+            $extensionName = substr($extensionName, 0, -3);
+        }
+        //
+        // Title translation
+        $translationPrefix = 'tx_' . strtolower($extensionName) . '_label.';
+        $translationKey = $translationPrefix . $translationKey;
+        $title = $this->translate($translationKey);
+        if ($title === null) {
+            throw new Exception('Translation key missing: ' . $translationKey);
+        }
+        //
+        // Prepare css classes
+        $classes = '';
+        if ($disabled) {
+            if (isset($_SERVER['DDEV_HOSTNAME'])) {
+                $classes = 'bg-danger';
+            } else {
+                $classes = 'd-none';
+            }
+        }
+        //
+        // Build button
+        switch ($type) {
+            case 'refresh':
+                $button = $buttonBar->makeLinkButton()
+                    ->setHref(GeneralUtility::getIndpEnv('REQUEST_URI'))
+                    ->setTitle($title)
+                    ->setClasses($classes)
+                    ->setIcon($this->iconFactory->getIcon('actions-refresh', Icon::SIZE_SMALL));
+                $buttonBar->addButton($button, ButtonBar::BUTTON_POSITION_RIGHT);
+                break;
+            case 'bookmark':
+                if ((int)GeneralUtility::makeInstance(Typo3Version::class)->getVersion() <= 10) {
+                    $button = $buttonBar->makeShortcutButton()
+                        ->setGetVariables(['id', 'M', $this->modulePrefix])
+                        ->setModuleName($this->moduleName)
+                        ->setDisplayName($title);
+                } else {
+                    $button = $buttonBar->makeShortcutButton()
+                        ->setArguments(['id', 'M', $this->modulePrefix])
+                        ->setRouteIdentifier($this->moduleName)
+                        ->setDisplayName($title);
+                }
+                $buttonBar->addButton($button, ButtonBar::BUTTON_POSITION_RIGHT);
+                break;
+            case 'new':
+                $parameter = [
+                    'returnUrl' => $this->getReturnUrl(),
+                    'id' => $this->pageUid,
+                    'edit' => [
+                        $table => [
+                            $this->pageUid => 'new'
+                        ]
+                    ]
+                ];
+                $button = $buttonBar->makeLinkButton()
+                    ->setHref((string)$this->uriBuilderBackend->buildUriFromRoute('record_edit', $parameter))
+                    ->setTitle($title)
+                    ->setClasses($classes)
+                    ->setIcon($this->iconFactory->getIcon('actions-document-new', Icon::SIZE_SMALL));
+                $buttonBar->addButton($button, ButtonBar::BUTTON_POSITION_LEFT);
+                break;
+            case 'edit':
+                if ($iconIdentifier === '') {
+                    $iconIdentifier = 'actions-document-open';
+                }
+                $parameter = [
+                    'returnUrl' => $this->getReturnUrl(),
+                    'id' => $this->pageUid,
+                    'edit' => [
+                        $table => [
+                            $uid => 'edit'
+                        ]
+                    ]
+                ];
+                $button = $buttonBar->makeLinkButton()
+                    ->setHref((string)$this->uriBuilderBackend->buildUriFromRoute('record_edit', $parameter))
+                    ->setTitle($title)
+                    ->setClasses($classes)
+                    ->setIcon($this->iconFactory->getIcon($iconIdentifier, Icon::SIZE_SMALL));
+                $buttonBar->addButton($button, ButtonBar::BUTTON_POSITION_LEFT);
+                break;
+            case 'csv':
+                $parameter = [
+                    'id' => $this->pageUid,
+                    $this->modulePrefix => [
+                        'action' => $action,
+                        'controller' => $controller,
+                        'csv' => 1,
+                    ]
+                ];
+                $uri = $this->uriBuilder->setArguments($parameter)->buildBackendUri();
+                $button = $buttonBar->makeLinkButton()
+                    ->setHref($uri)
+                    ->setTitle($title)
+                    ->setClasses($classes)
+                    ->setIcon($this->iconFactory->getIcon('actions-document-export-csv', Icon::SIZE_SMALL));
+                $buttonBar->addButton($button, ButtonBar::BUTTON_POSITION_LEFT);
+                break;
+            case 'action':
+                $parameter = [
+                    'id' => $this->pageUid,
+                    $this->modulePrefix => [
+                        'action' => $action,
+                        'controller' => $controller,
+                    ]
+                ];
+                $uri = $this->uriBuilder->setArguments($parameter)->buildBackendUri();
+                $button = $buttonBar->makeLinkButton()
+                    ->setHref($uri)
+                    ->setTitle($title)
+                    ->setClasses($classes)
+                    ->setIcon($this->iconFactory->getIcon($iconIdentifier, Icon::SIZE_SMALL));
+                $buttonBar->addButton($button, ButtonBar::BUTTON_POSITION_LEFT);
+                break;
+        }
+        return $buttonBar;
+    }
+
+    protected function setMetaInformation(string $table, int $uid): void
+    {
+        $metaRecord = BackendUtility::getRecord($table, $uid);
+        $this->moduleTemplate->getDocHeaderComponent()->setMetaInformation($metaRecord);
+    }
+
+    /**
+     * Create action menu
+     *
+     * @param array<mixed> $actions
+     * @return void
+     */
+    protected function createMenuActions(array $actions)
+    {
+        $menu = $this->moduleTemplate->getDocHeaderComponent()->getMenuRegistry()->makeMenu();
+        $menu->setIdentifier($this->moduleName);
+        foreach ($actions as $action) {
+            $controller = $action['controller'] ?? 'Backend';
+            $active = (
+                $this->request->getControllerActionName() === $action['action']
+                && $this->request->getControllerName() === $controller
+            );
+            $item = $menu->makeMenuItem()
+                ->setTitle($action['label'])
+                ->setHref(
+                    $this->uriBuilder->reset()->uriFor(
+                        $action['action'],
+                        [],
+                        $controller
+                    )
+                )
+                ->setActive($active);
+            $menu->addMenuItem($item);
+        }
+        $this->moduleTemplate->getDocHeaderComponent()->getMenuRegistry()->addMenu($menu);
+    }
+
+    /**
+     * @return string
+     * @throws RouteNotFoundException
+     */
+    protected function getReturnUrl(): string
+    {
+        $parameter = [
+            'id' => $this->pageUid,
+            $this->modulePrefix => [
+                'action' => $this->request->getControllerActionName(),
+                'controller' => $this->request->getControllerName(),
+            ],
+        ];
+        return (string)$this->uriBuilderBackend->buildUriFromRoute($this->moduleName, $parameter);
+    }
+    protected function getIcon(string $key): Icon
+    {
+        return $this->iconFactory->getIcon($key, Icon::SIZE_SMALL);
+    }
+
+    /**
+     * v12 returns ModuleTemplate, v11 ViewInterface
+     *
+     * @return ModuleTemplate|ViewInterface
+     */
+    protected function getViewToUse()
+    {
+        if (method_exists($this->moduleTemplate, 'assign')) {
+            return $this->moduleTemplate;
+        }
+        return $this->view;
+    }
+
+    protected function renderViewToUse(): ResponseInterface
+    {
+        if (!$this->getViewToUse() instanceof ModuleTemplate) {
+            // v11
+            $this->moduleTemplate->setContent($this->getViewToUse()->render());
+            return $this->htmlResponse($this->moduleTemplate->renderContent());
+        }
+        return $this->htmlResponse($this->getViewToUse()->render());
+    }
+}
+
 	private function setDocHeader(string $active) {
    $buttonBar = $this->moduleTemplate->getDocHeaderComponent()->getButtonBar();
    $list = $buttonBar->makeLinkButton()
